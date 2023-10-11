b430d5e092435d04c199d2121b59ebed47f9ae77
bBenettiz <benettimotog4@gmail.com>
2023-10-11 18:45:57 -0300

    initial commit

---
+++ Clientes.sql
@@ -0,0 +1,2 @@
+-- SQLite
+create TABLE Clientes(ClienteId int, Endereco varchar(80), Cidade varchar(30), Regiao varchar(10), CodigoPostal varchar (10), Pais varchar (30), Telefone varchar(15));
---
+++ Pedidos.sql
@@ -0,0 +1,2 @@
+-- SQLite
+CREATE TABLE Pedidos( PedidoId int, EmpregadoId int, DataPedido varchar(10), Peso varchar(10), CodTransportadora int, PedidoClienteId int);
